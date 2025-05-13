aa7697324e688c804f8a1cd4d285f680d719b620
idanl <idanl@IdanLin-Port>
2025-05-13 14:09:43 +0300

    13/5 sedfgsef

---
+++ JavaFinalProj/src/dataStruct/PersonTree.java
@@ -0,0 +1,58 @@
+package dataStruct;
+import ruppinLand.Person;
+
+public class PersonTree {
+	TreeNode root;
+
+	public PersonTree() {
+		this.root = null;
+	}
+
+	public void addPerson(Person person) {
+		if (person == null) {
+			throw new IllegalArgumentException("Person cannot be null");
+		}
+		if (root == null) {
+			root = new TreeNode(person);
+		} else {
+			addPersonRec(root, person);
+		}
+		}
+	private void addPersonRec(TreeNode current, Person person) {
+		if (current == null) {
+			return;
+		}
+		if (person.getId() < current.getPerson().getId()) {
+			if (current.getLeft() == null) {
+				current.setLeft(new TreeNode(person));
+			} else {
+				addPersonRec(current.getLeft(), person);
+			}
+		} else if (person.getId() > current.getPerson().getId()) {
+			if (current.getRight() == null) {
+				current.setRight(new TreeNode(person));
+			} else {
+				addPersonRec(current.getRight(), person);
+			}
+		} else {
+			throw new IllegalArgumentException("Person with this ID already exists");
+		}
+	}
+
+	public Person searchPerson(int id) {
+		return searchPersonRec(root, id);
+	}
+
+	private Person searchPersonRec(TreeNode current, int id) {
+		if (current == null) {
+			return null;
+		}
+		if (id == current.getPerson().getId()) {
+			return current.getPerson();
+		} else if (id < current.getPerson().getId()) {
+			return searchPersonRec(current.getLeft(), id);
+		} else {
+			return searchPersonRec(current.getRight(), id);
+		}
+	}
+}
---
+++ JavaFinalProj/src/dataStruct/TreeNode.java
@@ -0,0 +1,43 @@
+package dataStruct;
+
+import ruppinLand.Person;
+
+public class TreeNode {
+	Person person;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(Person person) {
+		this.person = person;
+		this.left = null;
+		this.right = null;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+		
+	public TreeNode getLeft() {
+		return left;
+	}
+	
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+
+	public boolean isLeaf() {
+		return left == null && right == null;
+	}
+}
