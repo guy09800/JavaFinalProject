abd7a3d2bd4aaae6c3c49a6e9728608ffdd2d2e6
Itay <Itay@LAPTOP-4OUL5JBE>
2025-05-17 21:17:00 +0300

    Test

--- JavaFinalProj/src/personClass/Employee.java
+++ JavaFinalProj/src/personClass/Employee.java
@@ -18,7 +18,7 @@ public class Employee extends Person {
 	// constructor with parameters
 	public Employee(String id, String name, GregorianCalendar birthday,String role , double salary) {
 		super(id, name, birthday);
-		this.role = role;
+		this.role = role1;
 		this.salary = salary;
 	}
 
