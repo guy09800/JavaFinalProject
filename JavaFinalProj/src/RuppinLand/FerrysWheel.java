5bb8c993f5efe3444348ac7d820cd4877acb87a5
guy09800 <sahar.guy@gmail.com>
2025-05-29 17:58:32 +0300

    made the last scene, ferrysWheel

--- JavaFinalProj/src/RuppinLand/FerrysWheel.java
+++ JavaFinalProj/src/RuppinLand/FerrysWheel.java
@@ -6,4 +6,12 @@ public class FerrysWheel extends Ride {
 			GregorianCalendar nextMaintenanceTime) {
 		super(name, minAge, minHeight, maxCapacity, price, nextMaintenanceTime);
 	}
+	public void scene() throws Exception {
+		printLine("🎡 The Ferris wheel creaks to life as the sun begins to set...");
+		printLine("🌇 The sky glows orange while the first cabin slowly lifts off the ground.");
+		printLine("🕊️ Birds glide past at eye level as the ride gains height.");
+		printLine("💑 Couples point out tiny people below, laughing and taking photos.");
+		printLine("🧍 Visitors sway gently, mesmerized by the panoramic view.");
+
+	}
 }
